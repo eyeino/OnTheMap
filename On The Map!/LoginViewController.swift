885122bd2f41df9c8245d2779c3bafdb1e822c78
ian <macfarlane@temple.edu>
2016-08-01 12:11:38 -0400

    feat: Create and organize View Controllers in MVC pattern
    Create view controllers in the Storyboard and link to
    their respective .swift files.  Place files in groups to
    adhere to MVC pattern/style.

---
+++ On The Map!/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  On The Map!
+//
+//  Created by Ian MacFarlane on 8/1/16.
+//  Copyright © 2016 Ian MacFarlane. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
