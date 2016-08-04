e707ee3cbb0be40e4c4f4a830648d0e30cfed07b
ian <macfarlane@temple.edu>
2016-08-04 01:17:11 -0400

    feat: Enable table view to populate cells with StudentInformation

--- On The Map!/TableViewController.swift
+++ On The Map!/TableViewController.swift
@@ -8,17 +8,55 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UITableViewController {
 
+    var students: [StudentInformation] {
+        get {
+            let delegate = UIApplication.sharedApplication().delegate as? AppDelegate
+            if let students = delegate?.students {
+                return students
+            } else {
+                return [StudentInformation]()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.tableView.reloadData()
+        
+        
     }
+    
+    let reuseIdentifier = "StudentTableCell"
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier)!
+        let student = students[indexPath.row]
+        
+        // Set the name and image
+        cell.textLabel?.text = "\(student.firstName!) \(student.lastName!)"
+        cell.detailTextLabel?.text = student.mapString!
+
+        
+        return cell
+    }
+    
+    /*
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let detailController = self.storyboard!.instantiateViewControllerWithIdentifier("MemeDetailViewController") as! MemeDetailViewController
+        detailController.meme = memes[indexPath.row]
+        self.navigationController!.pushViewController(detailController, animated: true)
+        
+    }
+    */
 
 
 }
