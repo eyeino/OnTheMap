f69813293b2367437021bc8a7f8dcbc24683c801
ian <macfarlane@temple.edu>
2016-08-17 19:32:53 -0400

    feat: Add segue to PhotoCollectionVC

--- On The Map!/MapViewController.swift
+++ On The Map!/MapViewController.swift
@@ -181,7 +181,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         if pinView == nil {
             pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
             pinView!.canShowCallout = true
-            pinView!.pinColor = .Red
+            pinView!.pinTintColor = UIColor.redColor()
             pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
         }
         else {
